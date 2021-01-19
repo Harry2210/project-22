3750152345449c345c6efdaa1fc0a1782da1efd0
Harry2210 <76477415+Harry2210@users.noreply.github.com>
2021-01-19 15:35:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,6 +55,7 @@ function draw() {
 function keyPressed() {
  if (keyCode == DOWN_ARROW) {
 
+	 Matter.Body.setStatic(packageBody,false)
 	packageSprite.x= packageBody.position.x 
 	packageSprite.y= packageBody.position.y 
   }
